fd2ff6e2ef7eeed829affca85b637134a4458192
João Victor Teófilo Salgado <joao.teofilo@geotech4web.com.br>
2019-08-23 19:04:25 -0300

    Criação de classes e renders de poligonos e linhas.

---
+++ ios/RNEsriRouter.swift
@@ -0,0 +1,9 @@
+//
+//  RNEsriRouter.swift
+//  app
+//
+//  Created by GT4W on 8/23/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ ios/app/RNEsriMapViewManager.swift
@@ -0,0 +1,21 @@
+//
+//  RNEsriMapViewManager.swift
+//  app
+//
+//  Created by GT4W on 8/23/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import UIKit
+
+class RNEsriMapViewManager: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
