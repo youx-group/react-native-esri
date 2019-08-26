a47a5481cbddffdaec6835e55a8a576c325ffae2
leonardo.almeida <leonardoalmeida.gt4w@gmail.com>
2019-08-26 14:40:05 -0300

    Insere layer de municipios, altera metodo do initialMap

---
+++ AGSMapView.js
@@ -0,0 +1,162 @@
+//  Created by react-native-create-bridge
+
+import React from 'react'
+import { NativeEventEmitter, Platform, requireNativeComponent, NativeModules, UIManager, findNodeHandle, DeviceEventEmitter } from 'react-native'
+import PropTypes from 'prop-types'
+const AGSMap = requireNativeComponent('RNArcGISMapView', ArcGISMapView);
+
+
+class ArcGISMapView extends React.Component {
+  constructor(props) {
+    super(props);
+    var eventEmitter;
+    if (Platform.OS === 'ios') {
+      eventEmitter = new NativeEventEmitter(NativeModules.RNArcGISMapViewModule);
+    } else {
+      eventEmitter = DeviceEventEmitter;
+    }
+    eventEmitter.addListener('isRoutingChanged', this.props.onRoutingStatusUpdate);
+  }
+
+  // MARK: Props
+  static propTypes = {
+    basemapUrl: PropTypes.string,
+    initialMapCenter: PropTypes.arrayOf(PropTypes.object),
+    minZoom: PropTypes.number,
+    maxZoom: PropTypes.number,
+    rotationEnabled: PropTypes.bool,
+    routeUrl: PropTypes.string,
+    onOverlayWasAdded: PropTypes.func,
+    onOverlayWasRemoved: PropTypes.func,
+    onOverlayWasModified: PropTypes.func,
+    onMapDidLoad: PropTypes.func,
+    onMapMoved: PropTypes.func,
+    onSingleTap: PropTypes.func,
+  };
+
+  static defaultProps = {
+    initialMapCenter: [
+      { latitude: -30.304790, longitude: -53.286374 }
+    ],
+    minZoom: 0,
+    maxZoom: 0,
+    rotationEnabled: true,
+    basemapUrl: '',
+    onSingleTap: () => { },
+    onOverlayWasAdded: () => { },
+    onOverlayWasRemoved: () => { },
+    onOverlayWasModified: () => { },
+    onMapDidLoad: () => { },
+    onMapMoved: () => { },
+    onRoutingStatusUpdate: () => { },
+    routeUrl: '',
+  };
+
+  isRouting = false;
+
+  // MARK: Exposed native methods
+  showCallout = (args) => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.showCalloutViaManager,
+      [args]
+    );
+  };
+
+  recenterMap = (pointArray) => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.centerMapViaManager,
+      [pointArray]
+    );
+  }
+
+  addGraphicsOverlay = (overlayData) => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.addGraphicsOverlayViaManager,
+      [overlayData]
+    );
+  }
+
+  removeGraphicsOverlay = (overlayId) => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.removeGraphicsOverlayViaManager,
+      [overlayId]
+    );
+  }
+
+  addPointsToOverlay = (args) => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.addPointsToOverlayViaManager,
+      [args]
+    );
+  }
+
+  updatePointsOnOverlay = (args) => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.updatePointsInGraphicsOverlayViaManager,
+      [args]
+    );
+  }
+
+  removePointsFromOverlay = (args) => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.removePointsFromOverlayViaManager,
+      [args]
+    );
+  }
+
+  routeGraphicsOverlay = (args) => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.routeGraphicsOverlayViaManager,
+      [args]
+    );
+  }
+
+  getRouteIsVisible = (callback) => {
+    if (Platform.OS === 'ios') {
+      UIManager.dispatchViewManagerCommand(
+        findNodeHandle(this.agsMapRef),
+        UIManager.getViewManagerConfig('RNArcGISMapView').Commands.getRouteIsVisibleViaManager,
+        [callback]
+      );
+    } else {
+      NativeModules.RNArcGISMapViewManager.getRouteIsVisible(findNodeHandle(this.agsMapRef), callback);
+    }
+  };
+
+  setRouteIsVisible = (args) => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.setRouteIsVisibleViaManager,
+      [args]
+    );
+  }
+
+  // MARK: Render
+  render() {
+    return <AGSMap {...this.props} ref={e => this.agsMapRef = e} />
+  }
+
+  // MARK: Disposal
+  componentWillUnmount() {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.agsMapRef),
+      UIManager.getViewManagerConfig('RNArcGISMapView').Commands.dispose,
+      [args]
+    );
+  }
+}
+
+export const setLicenseKey = (string) => {
+  NativeModules.RNArcGISMapViewManager.setLicenseKey(string);
+};
+
+
+export default ArcGISMapView;
--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
+import ArcGISMapView from 'react-native-arcgis-mapview'
 
 const App = () => {
-  return <View style={{ flex: 1 }} />;
+  return (
+    <View style={{ flex: 1 }}>
+      <ArcGISMapView
+        ref={mapView => this.mapView = mapView}
+        style={{ flex: 1 }}
+        initialMapCenter={[{ latitude: -30.304790, longitude: -53.286374, initialZoom: 6 }]}
+        recenterIfGraphicTapped={true}
+        rotationEnabled={false}
+        mapBasemap={{ type: 'normal' }}
+      />
+    </View>
+  );
 };
 
 export default App;
